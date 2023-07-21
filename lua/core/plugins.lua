7276248cfaf6da0f10c99002669e02381421ac91
Scott Callaway <scott.callaway@uipath.com>
2023-07-21 14:54:51 +0100

    Install octo.nvim to test Github from nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -108,6 +108,17 @@ require("packer").startup(function()
             require("configs.gitsigns")
         end,
     })
+    use({
+        "pwntester/octo.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("octo").setup()
+        end,
+    })
 
     -- Searching
     use("nvim-telescope/telescope.nvim")
