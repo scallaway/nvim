ba20ed0f4c5487ad482d588dd550e2accf2989af
Scott Callaway <scott.callaway@uipath.com>
2023-07-21 14:55:27 +0100

    Update some configs for text-only files

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,2 +1,10 @@
 vim.opt.textwidth = 79
 vim.opt.shiftwidth = 2
+vim.opt.number = false
+vim.opt.relativenumber = false
+
+-- Disable CMP in this file
+local cmp = require("cmp")
+cmp.setup.buffer({
+    sources = { ... },
+})
