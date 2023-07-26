58c39dfb5a8ebce9581ef1e8f0c8b904ae9a6ec0
Scott Callaway <scott.callaway@uipath.com>
2023-07-26 15:04:36 +0100

    Turn off spelling because it needs more config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,2 @@
 vim.opt.textwidth = 79
 vim.opt.shiftwidth = 2
-vim.opt.spell = true
-vim.opt.spelllang = "en_gb"
--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -2,8 +2,6 @@ vim.opt.textwidth = 79
 vim.opt.shiftwidth = 2
 vim.opt.number = false
 vim.opt.relativenumber = false
-vim.opt.spell = true
-vim.opt.spelllang = "en_gb"
 
 -- Disable CMP in this file
 local cmp = require("cmp")
