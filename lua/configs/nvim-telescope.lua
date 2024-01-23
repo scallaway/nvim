29b538d588a3991fe15f547f9ad115355decca54
Scott Callaway <github@scottcallaway.co.uk>
2024-01-23 09:09:10 +0000

    add buffer search for telescope and stylua cleanup

--- lua/configs/nvim-telescope.lua
+++ lua/configs/nvim-telescope.lua
@@ -13,6 +13,10 @@ vim.keymap.set("n", "<C-f>", function()
     -- Use Ctrl-F to ripgrep through files
     require("telescope.builtin").live_grep()
 end)
+vim.keymap.set("n", "<C-b>", function()
+    -- Use <Leader>fb to search through buffers
+    require("telescope.builtin").buffers()
+end)
 
 telescope.setup({
     pickers = {
@@ -23,13 +27,23 @@ telescope.setup({
                 height = 10,
             },
         },
+        buffers = {
+            theme = "ivy",
+            previewer = false,
+            layout_config = {
+                height = 10,
+            },
+        },
         live_grep = {
             layout_config = {
                 width = 0.75,
                 height = 0.8,
-                preview_width = 100,
+                preview_width = 80,
             },
-            vimgrep_arguments = table.insert(conf.vimgrep_arguments, "--fixed-strings"),
+            vimgrep_arguments = table.insert(
+                conf.vimgrep_arguments,
+                "--fixed-strings"
+            ),
         },
     },
     defaults = {
