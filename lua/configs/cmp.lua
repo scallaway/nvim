55efcb0f7ce47c6547f866768d89e72ec06084db
Scott Callaway <github@scottcallaway.co.uk>
2024-05-02 11:36:17 +0100

    redo the cmp configuration slightly
    This adds some more integrations to cmp to allow for better prompts in
    git files, as well as cleaning up the configuration file a bit by
    applying stylua to it.

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,109 +1,121 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-    return
-end
-
-local luasnip_ok, luasnip = pcall(require, "luasnip")
-if not luasnip_ok then
-    return
+  return
 end
 
 local lspkind_ok, lspkind = pcall(require, "lspkind")
 if not lspkind_ok then
-    return
+  return
 end
 
 local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+    and vim.api
+        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match("%s")
+      == nil
 end
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-    mapping = {
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, {
-            "i",
-            "s",
-        }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, {
-            "i",
-            "s",
-        }),
-    },
-    formatting = {
-        format = lspkind.cmp_format({
-            -- show only symbol annotations
-            mode = "symbol",
-            -- prevent the popup from showing more than provided characters
-            maxwidth = 50,
-            -- when popup menu exceed maxwidth, the truncated part would show
-            -- ellipsis_char instead (must define maxwidth first)
-            ellipsis_char = "...",
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      -- show only symbol annotations
+      mode = "symbol",
+      -- prevent the popup from showing more than provided characters
+      maxwidth = 50,
+      -- when popup menu exceed maxwidth, the truncated part would show
+      -- ellipsis_char instead (must define maxwidth first)
+      ellipsis_char = "...",
 
-            -- The function below will be called before any actual
-            -- modifications from lspkind so that you can provide more controls
-            -- on popup customization.
-            -- (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            before = function(_, vim_item)
-                return vim_item
-            end,
-        }),
+      -- The function below will be called before any actual
+      -- modifications from lspkind so that you can provide more controls
+      -- on popup customization.
+      -- (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(_, vim_item)
+        return vim_item
+      end,
+    }),
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "path" },
+  }, {
+    { name = "buffer" },
+  }),
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  view = {
+    entries = {
+      name = "custom",
+      selection_order = "near_cursor",
     },
-    sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-        { name = "buffer", group_index = 3 },
-    },
-    confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-    },
-    view = {
-        entries = {
-            name = "custom",
-            selection_order = "near_cursor",
-        },
-    },
-    experimental = {
-        ghost_text = false,
-    },
+  },
+  experimental = {
+    ghost_text = false,
+  },
 })
 
--- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+  sources = cmp.config.sources({
+    { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+local autopairs_ok, cmp_autopairs =
+  pcall(require, "nvim-autopairs.completion.cmp")
+
+if not autopairs_ok then
+  return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -75,12 +75,14 @@ local plugins = {
       require("configs.cmp")
     end,
   },
-  "L3MON4D3/LuaSnip",
+  -- "L3MON4D3/LuaSnip",
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/cmp-nvim-lua",
+  "petertriho/cmp-git",
+  "saadparwaiz1/cmp_luasnip",
   {
     "onsails/lspkind.nvim",
     config = function()
