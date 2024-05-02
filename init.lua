1451595da6f2bb0ea0106be4ed24c75f0076d411
Scott Callaway <github@scottcallaway.co.uk>
2024-05-02 11:38:41 +0100

    format init.lua file

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 vim.loader.enable()
 
 for _, source in ipairs({
-    "core.base",
-    "core.plugins",
-    "core.theme",
-    "core.mappings",
+  "core.base",
+  "core.plugins",
+  "core.theme",
+  "core.mappings",
 }) do
-    local status_ok, fault = pcall(require, source)
-    if not status_ok then
-        vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-    end
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then
+    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+  end
 end
