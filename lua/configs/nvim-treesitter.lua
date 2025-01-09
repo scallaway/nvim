850129e004a55cf48a2ff5a7551fa5416e191b3b
Scott Callaway <github@scottcallaway.co.uk>
2025-01-09 17:49:32 +0000

    Fix some formatting

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,53 +1,65 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-    return
+  return
 end
 
 treesitter.setup({
-    ensure_installed = {
-        "css",
-        "gitcommit",
-        "gitignore",
-        "javascript",
-        "python",
-        "rust",
-        "typescript",
-        "org",
+  ensure_installed = {
+    "css",
+    "gitcommit",
+    "gitignore",
+    "javascript",
+    "python",
+    "rust",
+    "typescript",
+    "org",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+    config = {
+      javascript = {
+        __default = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        jsx_attribute = "// %s",
+        comment = "// %s",
+      },
+      typescript = {
+        __default = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        jsx_attribute = "// %s",
+        comment = "// %s",
+      },
     },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
+  },
+  rainbow = {
+    enable = true,
+    disable = { "html" },
+    extended_mode = false,
+    max_file_lines = nil,
+  },
+  autopairs = { enable = true },
+  autotag = { enable = true },
+  incremental_selection = { enable = true },
+  indent = { enable = false },
+  textsubjects = {
+    enable = true,
+    prev_selection = ",", -- (Optional) keymap to select the previous selection
+    keymaps = {
+      ["."] = "textsubjects-smart",
+      [";"] = "textsubjects-container-outer",
+      ["i;"] = {
+        "textsubjects-container-inner",
+        desc = "Select inside containers (classes, functions, etc.)",
+      },
     },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-        config = {
-            javascript = {
-                __default = "// %s",
-                jsx_element = "{/* %s */}",
-                jsx_fragment = "{/* %s */}",
-                jsx_attribute = "// %s",
-                comment = "// %s",
-            },
-            typescript = {
-                __default = "// %s",
-                jsx_element = "{/* %s */}",
-                jsx_fragment = "{/* %s */}",
-                jsx_attribute = "// %s",
-                comment = "// %s",
-            },
-        },
-    },
-    rainbow = {
-        enable = true,
-        disable = { "html" },
-        extended_mode = false,
-        max_file_lines = nil,
-    },
-    autopairs = { enable = true },
-    autotag = { enable = true },
-    incremental_selection = { enable = true },
-    indent = { enable = false },
+  },
 })
