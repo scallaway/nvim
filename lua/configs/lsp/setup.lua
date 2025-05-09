fe9cec06a17d11debc4c0d8fcdfd58cdf5b790e2
Scott Callaway <github@scottcallaway.co.uk>
2025-05-09 14:30:10 +0100

    Re-add key binds for LSP
    Should probably clean up the rest of this file, but I want to keep the
    existing commands around in case I need to re-add them.

--- lua/configs/lsp/setup.lua
+++ lua/configs/lsp/setup.lua
@@ -24,6 +24,11 @@ local on_attach = function(_, bufnr)
   utils.buf_map(bufnr, "n", "gL", ":vsp<CR>:LspDef<CR>")
 end
 
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<Leader>a", vim.diagnostic.open_float)
+
 for server, config in pairs(require("configs.lsp.servers")) do
   vim.lsp.config(server, config)
   vim.lsp.enable(server)
